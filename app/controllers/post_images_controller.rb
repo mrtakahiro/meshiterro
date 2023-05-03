a75876657f9e35d9a0939043f92975054718fa78
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-05-03 12:40:39 +0000

    update

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,12 +3,14 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
-  #投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -17,6 +19,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
